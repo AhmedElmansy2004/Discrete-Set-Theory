5ce997737e0e3a44c45abdc3e0e7d53e0d36023b
Ahmed El Mansy <ahmedmansi2004@gmail.com>
2025-12-05 09:23:39 +0300

    feat: added some frontend but there are still a lot of bugs to solve

--- Frontend/my-app/src/app/app.ts
+++ Frontend/my-app/src/app/app.ts
@@ -12,7 +12,10 @@ export class App {
   protected readonly title = signal('my-app');
 
   numOfUElements: number = 2;
-  universalSet: string[][] = [];
+  universalSet: string[] = [];
+  numberOfSubsets: number = 0;
+  subsets: string[][] = [];
+  available: string[] = [...this.universalSet];
 
   getRange(n: number) {
     return Array.from({length: n}, (_, i) => i);
@@ -20,7 +23,7 @@ export class App {
 
   increaseNumber(count: number){
     switch (count){
-      case 1:
+      case 0:
         this.numOfUElements++;
         console.log(this.numOfUElements)
         break;
@@ -35,13 +38,22 @@ export class App {
 
   decreaseNumber(count: number){
     switch (count){
-      case 1:
-        this.numOfUElements--;
+      case 0:
+        if(this.numOfUElements > 0) this.numOfUElements--;
         break;
 
       default:
         break;
     }
 
+  }
+
+  setAvailable(index: number){
+    this.available.splice(index, 1);
+  }
+
+  resetAvailable(): string{
+    this.available = [...this.universalSet];
+    return "";
   }
 }
